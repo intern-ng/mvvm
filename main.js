a8bab8b2166645b39a81cfbc59123cb07b5f6890
trinity <imandry.c@gmail.com>
2014-04-15 01:00:28 +0800

    ui: update

--- main.js
+++ main.js
@@ -7,9 +7,22 @@ define({
 
     var electron = require('electron'), _electron = electron, kit = electron.toolkit;
 
+    var CallAssigner = function (attr, value) {
+      this[attr].call(this, value);
+    };
+
+    var Assigner$Val = function (attr, value) {
+      this.val(value);
+      this.trigger('input');
+    };
+
     with (kit) electron()
     .pipe(TextInput('input[name="title"]'))
-    .pipe(Bypass, ObjectWriter(document, { title: 'text' }))
+    .pipe(Bypass,
+          ObjectWriter(document, { title: 'text' }),
+          ObjectWriter($('input[name="mirror"]'), {
+            val: 'text'
+          }, CallAssigner))
     .attach()
     .activate();
 
@@ -20,6 +33,14 @@ define({
         return (text) ? 'MVVM Demonstration - ' + text : 'MVVM Demonstration';
       }
     }))
+    .attach()
+    .activate();
+
+    _electron()
+    .pipe(kit.TextInput('input[name="mirror"]'))
+    .pipe(kit.ObjectWriter($('input[name="title"]'), {
+      val: 'text'
+    }, Assigner$Val))
     .attach()
     .activate();
 
