8baae61daefde183f00d7622f78acb1e9c82a48b
trinity <imandry.c@gmail.com>
2014-04-14 23:15:01 +0800

    ui: demonstration

--- main.js
+++ main.js
@@ -3,44 +3,13 @@ define({
 
   main: function (global, module, exports, require) {
 
-    var mvvm = require('electron');
-    var $ = require('jquery');
+    var electron = require('electron'), kit = electron.toolkit;
 
-    var A = function (opt) {
-      return function (use) {
-        var $textbox = $('input');
-        var _this = this;
-        var handler = function () {
-          _this.resolve({ m: $textbox.val() });
-        };
-        this
-        .on('attach', function () {
-          $('input').keyup(handler);
-        }).on('detach', function () {
-          $('input').off('keyup', handler);
-        });
-      };
-    }, B = function (use) {
-      return use(function () {
-        this.resolve({ n: 2 });
-      });
-    }, H = function (use) {
-      return use(function (m, n) {
-        document.title = m;
-        this.resolve({ a: m, b: n });
-      });
-    }, F = function (use) {
-      return use(function (___) {
-        this.resolve(___);
-      });
-    };
-
-    mvvm()
-    .pipe(A(), B)
-    .pipe(H)
-    .pipe(F)
+    electron()
+    .pipe(kit.TextInput('input[name="title"]'))
+    .pipe(kit.Bypass, kit.ObjectWriter(document, { title: 'text' }))
     .attach()
-    .exec();
+    .activate();
 
   }
 
